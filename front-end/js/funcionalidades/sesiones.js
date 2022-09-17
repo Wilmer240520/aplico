b25c8def59cbd6eb08f2d99bd53e9dbf85744576
Wilmer240520 <87386016+Wilmer240520@users.noreply.github.com>
2022-09-17 14:12:13 -0500

    Delete front-end/js directory

--- front-end/js/funcionalidades/sesiones.js
+++
@@ -1,9 +0,0 @@
-if(localStorage.getItem('token') !== null){
-    if(localStorage.getItem('usuario') !== null){
-        if(localStorage.getItem('usuario') == 's'){
-            location.href = "paginas/super-usuario/administracion.html"
-        }else{
-            location.href = "paginas/administrador/administracion.html"
-        }
-    }
-}
